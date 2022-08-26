2e9191e3277c3bfb0a7d931a56800776c1326756
Sahil Girotra <sahilgirotra52@gmail.com>
2022-08-26 14:49:52 +0530

    first commit

---
+++ q2.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+ 
+bool isConsecutive(int arr[], int n)
+{
+    // base case
+    if (n <= 1) {
+        return true;
+    }
+ 
+    int min = arr[0], max = arr[0];
+ 
+
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+ 
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+ 
+   
+    if (max - min != n - 1) {
+        return false;
+    }
+ 
+    
+    unordered_set<int> visited;
+ 
+    
+    for (int i = 0; i < n; i++)
+    {
+        
+        if (visited.find(arr[i]) != visited.end()) {
+            return false;
+        }
+ 
+        
+        visited.insert(arr[i]);
+    }
+ 
+    
+    return true;
+}
+ 
+int main()
+{
+    int arr[100];
+    int n;
+ cin>>n;
+ 
+ for(int i=0;i<n;i++){
+     cin>>arr[i];
+ }
+isConsecutive(arr, n)? cout << "The array contains consecutive integers":
+                        cout << "The array does not contain consecutive integers";
+ 
+    return 0;
+}
